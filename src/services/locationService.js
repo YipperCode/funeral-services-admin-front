82333ff4227be04f75a931a636241cec142cd888
Yipper Lenovo <yipergamerpro@gmail.com>
2025-01-18 16:06:01 -0600

    Add front uri

--- src/services/locationService.js
+++ src/services/locationService.js
@@ -1,20 +1,17 @@
 export const getLocationByIP = async () => {
-    try {
-      const response = await fetch("http://www.geoplugin.net/json.gp", {
-        headers: {
-          "Origin": "https://funeral-services-admin-front.onrender.com/"
-        }
-      });
-      const data = await response.json();
-      return {
-        city: data.geoplugin_city,
-        country: data.geoplugin_countryName,
-        latitude: data.geoplugin_latitude,
-        longitude: data.geoplugin_longitude,
-      };
-    } catch (error) {
-      console.error("Error fetching location:", error);
-      return null;
-    }
-  };
-  
+  try {
+    const response = await fetch(
+      `http://api.ipstack.com/check?access_key=YOUR_ACCESS_KEY`
+    );
+    const data = await response.json();
+    return {
+      city: data.city,
+      country: data.country_name,
+      latitude: data.latitude,
+      longitude: data.longitude,
+    };
+  } catch (error) {
+    console.error("Error fetching location:", error);
+    return null;
+  }
+};
